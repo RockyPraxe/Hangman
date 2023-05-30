c9709026fcfb639c46114fac41cc69358fade421
RockyPraxe <filipio.svanda@gmail.com>
2023-05-30 14:01:53 +0000

    Add first function with welcome message and name input

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import random
 
-print('Welcome to hangman')
-name = input('Enter your name: ')
-print(f'Hello, {name}! Lets play Hangman.')
+def welcome_message():
+    print('Welcome to hangman')
+    name = input('Enter your name: ')
+    print(f'Hello, {name}! Lets play Hangman.')
+
+welcome_message()    
+
