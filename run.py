44eb2ca55b849964ef90c9d28f8cab121e68008e
RockyPraxe <filipio.svanda@gmail.com>
2023-05-30 13:58:00 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+print('Welcome to hangman')
+name = input('Enter your name: ')
+print(f'Hello, {name}! Lets play Hangman.')
