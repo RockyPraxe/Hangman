ed86ea935d3ab5fc5ee10351c2868a66a421a474
RockyPraxe <filipio.svanda@gmail.com>
2023-05-30 14:17:11 +0000

    method choice for choosing random word

--- run.py
+++ run.py
@@ -1,9 +1,32 @@
 import random
 
+
 def welcome_message():
+    """
+    Input user name and welcome message
+    """
     print('Welcome to hangman')
-    name = input('Enter your name: ')
-    print(f'Hello, {name}! Lets play Hangman.')
+    name = input('Enter your name: \n')
+    print(f'Hello, {name}! Lets play Hangman!')
+    print('---------------------------------')
 
 welcome_message()    
+
+
+wordDictionary = [
+    "apple", "banana", "cat", "dog", "elephant",
+    "fish", "grape", "horse", "ice cream", "jungle",
+    "koala", "lion", "monkey", "nest", "orange",
+    "penguin", "queen", "rabbit", "snake", "tiger",
+    "unicorn", "violet", "whale", "xylophone", "yak",
+    "zebra", "ant", "butterfly", "crocodile", "dragon"
+]
+
+
+### Choose a random word with choice method
+randomWord = random.choice(wordDictionary)
+### Now we can print undescore lines and user can start guessing
+for x in randomWord:
+    print('_', end=' ')
+
 
