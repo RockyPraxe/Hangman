f28682edfd8901498e3bd5d38e94dbd14a713ef3
RockyPraxe <filipio.svanda@gmail.com>
2023-05-31 17:23:29 +0000

    indentation and whitespaces changes after pep8 python validator

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import random
 
+
 def welcome_message():
     """
     Input user name and welcome message
@@ -8,6 +9,7 @@ def welcome_message():
     name = input('Enter your name: \n').upper()
     print(f'Hello, {name}! Lets play Hangman!')
     print('---------------------------------')
+
 
 welcome_message()
 
@@ -22,11 +24,11 @@ wordDictionary = [
 ]
 
 
-### Choose a random word with choice method
+# Choose a random word with choice method
 
 randomWord = random.choice(wordDictionary)
 
-### Now we can print undescore lines and user can start guessing
+# Now we can print undescore lines and user can start guessing
 
 for x in randomWord:
     print('_', end=' ')
@@ -36,72 +38,74 @@ def print_hangman(wrong):
     """
     Function for hangman design
     with elif statements
-    """  
-    if(wrong == 0):
+    """
+    if (wrong == 0):
         print('\n+---+')
         print('    |')
         print('    |')
         print('    |')
         print('   ===')
-    elif(wrong ==1):
+    elif (wrong == 1):
         print('\n+---+')
         print('O   |')
         print('    |')
         print('    |')
         print('   ===')
-    elif(wrong ==2):
+    elif (wrong == 2):
         print('\n+---+')
         print('O   |')
         print('|   |')
         print('    |')
-        print('   ===')  
-    elif(wrong ==3):
+        print('   ===')
+    elif (wrong == 3):
         print('\n+---+')
         print(' O  |')
         print('/|  |')
         print('    |')
-        print('   ===') 
-    elif(wrong ==4):
+        print('   ===')
+    elif (wrong == 4):
         print('\n+---+')
         print(' O  |')
         print('/|\ |')
         print('    |')
-        print('   ===')  
-    elif(wrong ==5):
+        print('   ===')
+    elif (wrong == 5):
         print('\n+---+')
         print(' O  |')
         print('/|\ |')
         print('/   |')
-        print('   ===')   
-    elif(wrong ==6):
+        print('   ===')
+    elif (wrong == 6):
         print('\n+---+')
         print(' O  |')
         print('/|\ |')
         print('/ \ |')
-        print('   ===')  
+        print('   ===')
 
 
 def printWord(guessedLetters):
     """
-      Prints the word to guess with guessed letters filled in and empty spaces for unguessed letters.
+      Prints the word to guess with guessed letters filled in
+      and empty spaces for unguessed letters.
       Returns the number of correctly guessed letters.
     """
     counter = 0
     rightLetters = 0
     for char in randomWord:
-        if(char in guessedLetters):
+        if (char in guessedLetters):
             print(randomWord[counter], end=' ')
-            rightLetters +=1
+            rightLetters += 1
         else:
             print(' ', end=' ')
-        counter +=1
-    return rightLetters        
+        counter += 1
+    return rightLetters
 
 
 def printLines():
     """
     Prints lines to represent the length of the word to guess.
-    This function prints lines using the Unicode character '\u203E' to represent the length of the word to guess.
+    This function prints lines using the Unicode character '\u203E'
+    to represent the length of the word to guess.
     Each line represents an unknown letter in the word.
     """
     print('\r')
@@ -109,15 +113,18 @@ def printLines():
         print('\u203E', end=' ')
 
 
-### Creating the loop which will operating the game
+# Creating the loop which will operating the game
 
 def hangman_game():
     """
         Main loop for the Hangman game.
 
-        This loop continues until the player has either guessed the word correctly or made 6 wrong guesses.
-        It prompts the user to guess a letter, checks if the letter is correct or wrong, updates the game state accordingly,
-        and prints the current hangman picture, the guessed letters, the word with guessed letters filled in,
+        This loop continues until the player has either guessed
+        the word correctly or made 6 wrong guesses.
+        It prompts the user to guess a letter, checks if the letter
+        is correct or wrong, updates the game state accordingly,
+        and prints the current hangman picture, the guessed letters,
+        the word with guessed letters filled in,
         and lines representing unknown letters.
     """
     length_of_word_to_guess = len(randomWord)
@@ -126,55 +133,51 @@ def hangman_game():
     current_letters_guessed = []
     current_letters_right = 0
 
-
-
-
-    while(wrong_answers != 6 and current_letters_right != length_of_word_to_guess):
+    while (wrong_answers != 6 and current_letters_right != length_of_word_to_guess):
         print('\nLetters guessed are: ')
         for letter in current_letters_guessed:
             print(letter, end=' ')
 
-        ### Promt user input
+        # Promt user input
 
         try:
-            ### Error will be raised if input is incorrect
+            # Error will be raised if input is incorrect
             letterGuessed = input('\nGuess a letter please: ')
 
             if not letterGuessed.isalpha() or len(letterGuessed) != 1 or letterGuessed != letterGuessed.lower():
                 raise ValueError('Invalid input. You must enter a single lower letter.')
 
+            # User is right
 
-            ### User is right
-
-            if(randomWord[current_guess_index] == letterGuessed):
+            if (randomWord[current_guess_index] == letterGuessed):
                 print_hangman(wrong_answers)
-                ### Print word
-                current_guess_index +=1
+                # Print word
+                current_guess_index += 1
                 current_letters_guessed.append(letterGuessed)
                 current_letters_right = printWord(current_letters_guessed)
                 printLines()
 
-            ### User was wrong
+            # User was wrong
 
             else:
-                wrong_answers +=1
+                wrong_answers += 1
                 current_letters_guessed.append(letterGuessed)
 
-                ### Update the picture
+                # Update the picture
 
                 print_hangman(wrong_answers)
 
-                 ### Print word
+                # Print word
 
                 current_letters_right = printWord(current_letters_guessed)
                 printLines()
 
         except ValueError as e:
             print(f"{str(e)}")
-            continue        
+            continue
 
-    print('\nGame is over! \nThank you for playing.')   
-          
+    print('\nGame is over! \nThank you for playing.')
+
 
 hangman_game()
 
@@ -187,7 +190,6 @@ def restart_game():
         hangman_game()
         if not play_again():
             break
-
 
 
 def play_again():
@@ -205,7 +207,5 @@ def play_again():
             print('Invalid input. Please enter \'y\' or \'n\'.')
 
 
-
 play_again()
-restart_game() 
-                                   
+restart_game()
